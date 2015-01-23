19bba3c2b2e23a2ceb9baac41e1c77eec6f0daf8
Guillem Anguera <ganguera@gmail.com>
2015-01-23 16:06:20 +0100

    Use primary Google's NS

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ apt-get remove -y --purge puppet puppet-common chef chef-zero
 apt-get autoremove -y
 
 DOCKER_IP="$(ifconfig docker0 | awk -F ' *|:' '/inet addr/{print $4}')"
-sed -i "s/#DOCKER_OPTS=\"--dns 8.8.8.8 --dns 8.8.4.4\"/DOCKER_OPTS=\"--dns ${DOCKER_IP} --dns 8.8.4.4 --dns-search service.consul\"/" /etc/default/docker
+sed -i "s/#DOCKER_OPTS=\"--dns 8.8.8.8 --dns 8.8.4.4\"/DOCKER_OPTS=\"--dns ${DOCKER_IP} --dns 8.8.8.8 --dns-search service.consul\"/" /etc/default/docker
 service docker restart
 
 NODE_IP="$(ifconfig eth1 | awk -F ' *|:' '/inet addr/{print $4}')"
